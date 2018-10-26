b1bfb1a31760c3072c08c7ceaa9fc8a028e8c262
RyokuH <42228052+RyokuH@users.noreply.github.com>
2018-10-27 00:17:37 +1100

    Update PathTraverse.m

--- Ass3PathPlanning/PathTraverse.m
+++ Ass3PathPlanning/PathTraverse.m
@@ -1,34 +1,68 @@
-start = [1 1];
-goal = [3 1];
-obstacles = [2 1; 2 2; 2 3];
+function PathTraverse(start,goal,obstacles)
 
+% Examples
+
+% Easy
+start = [9 1];
+goal = [7 4];
+obstacles = [8 2; 8 3];
+
+% Medium
+start = [9 1];
+goal = [8 6];
+obstacles = [8 2; 8 3; 7 5; 7 6; 8 5; 9 5];
+ 
+% % Hard
+% start = [9 1];
+% goal = [8 6];
+% obstacles = [8 2; 8 3; 7 5; 7 6; 8 5; 9 5];
+% 
+% % No Path
+% start = [9 1];
+% goal = [8 6];
+% obstacles = [8 2; 8 3; 7 5; 7 6; 8 5; 9 5];
+
+% just for visualising - comment out when not needed
+board = ones(9,9);
+board(start(1),start(2)) = 2;
+board(goal(1),goal(2)) = 3;
+for i = 1:length(obstacles)
+    board(obstacles(i,1), obstacles(i,2)) = 4;
+end
+imagesc(board);
+
+% generate path 
 path = PathPlan(start, goal, obstacles);
 
-if ~path
+if path ~= 0
     % path exists
     [x,y] = BP2Coord(start);
     command = sprintf('MVPOSTAB %f,%f,5',x,y);
+    %disp(command) % for debugging
+    
     fwrite(socket, command);
     str = ReceiveString(socket);
     inMotion = checkMotion(socket);
     
     while(inMotion)
-        pause(0.1);
-        inMotion = checkMotion(socket);
+       pause(0.1);
+       inMotion = checkMotion(socket);
     end
     
     pathCounter = 1;
-    [row,~] = size(obstacles);
+    [row,~] = size(path);
     while pathCounter <= row
-        [x,y] = BP2Coord(obstacles(row,:));
+        [x,y] = BP2Coord(path(pathCounter,:));
         command = sprintf('MVPOSTAB %f,%f,5',x,y);
+        % disp(command) % for debugging
+        
         fwrite(socket, command);
         str = ReceiveString(socket);
         inMotion = checkMotion(socket);
 
         while(inMotion)
-            pause(0.1);
-            inMotion = checkMotion(socket);
+           pause(0.1);
+           inMotion = checkMotion(socket);
         end
         
         pathCounter = pathCounter + 1;
@@ -38,7 +72,15 @@ else
     % no path 
 end
 
+end
+
 % either reached goal or no path
+
+function [x,y] = BP2Coord(bp)
+    % dummy for now
+    x = bp(1);
+    y = bp(2);
+end
 
 function [str] =  ReceiveString(socket)
     data = fgetl(socket);
